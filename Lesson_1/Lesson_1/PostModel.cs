1b3a96f4ad7408f56fe3d422f1f60c45ba2deb34
yurec0098 <yurec0098@rambler.ru>
2022-02-03 06:48:29 +0200

    Консольное приложение, которое выполнит асинхронно 10 HTTP-запросов на получение постов из блога, по целочисленному идентификатору, начиная с id=4 по id=13. Для выполнения запросов необходимо использовать HttpClient (см. https://docs.microsoft.com/ru-ru/dotnet/api/system.net.http.httpclient?view=net-5.0).
    В результате работы программы должен получиться текстовый файл под названием result.txt

---
+++ Lesson_1/Lesson_1/PostModel.cs
@@ -0,0 +1,12 @@
+﻿namespace Lesson_1
+{
+	public class PostModel
+	{
+		public override string ToString() => $"{userId}\n{id}\n{title}\n{body}\n";
+
+		public uint userId { get; set; }
+		public uint id { get; set; }
+		public string title { get; set; }
+		public string body { get; set; }
+	}
+}
--- Lesson_1/Lesson_1/Program.cs
+++ Lesson_1/Lesson_1/Program.cs
@@ -6,7 +6,37 @@ namespace Lesson_1
 	{
 		public static void Main(string[] args)
 		{
+			var tasks = new List<Task<PostModel>>();
+			for (uint i = 4; i <= 13; i++)
+				tasks.Add(GetPostByID(i));
 
+			Task.WaitAll(tasks.ToArray());
+
+			using (var sw = new StreamWriter(File.Create("result.txt")))
+			{
+				foreach (var task in tasks)
+					if(task.IsCompleted && task.Exception == null)
+						sw.WriteLine(task.Result);
+			}
+		}
+
+		private static async Task<PostModel> GetPostByID(uint postID)
+		{
+			HttpClient client = new HttpClient();
+			var response = await client.GetAsync($"https://jsonplaceholder.typicode.com/posts/{postID}");
+
+			if (response.IsSuccessStatusCode)
+			{
+				await using var responseStream = await response.Content.ReadAsStreamAsync();
+				if(await JsonSerializer.DeserializeAsync<PostModel>(responseStream, new JsonSerializerOptions(JsonSerializerDefaults.Web)) is PostModel post)
+					return post;
+				else
+					throw new Exception($"Error content");
+			}
+			else
+			{
+				throw new Exception($"Eroor code: {response.StatusCode}");
+			}
 		}
 	}
 }
